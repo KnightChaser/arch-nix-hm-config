053f07b322ca0f87bf8b19cfa58f185536fc9049
knightchaser <agerio100@naver.com>
2025-11-16 00:42:34 +0900

    Refactor: Modularize nix-shipped starship.rs configuration toml file

--- modules/prompt.nix
+++ modules/prompt.nix
@@ -1,17 +1,10 @@
 # modules/prompt.nix
 
 {
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = false;
-      character = {
-        success_symbol = "[➜](bold green) ";
-        error_symbol = "[➜](bold red) ";
-      };
-      # you can add more customizations
-      format = "$all$directory$git_branch$git_state$character";
-    };
+  programs.starship = { enable = true; };
+
+  home.file = {
+    ".config/starship.toml".source = ../dotfiles/starship/starship.toml;
   };
 }
 
