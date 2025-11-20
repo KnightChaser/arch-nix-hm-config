07cefe68c03e0c5be1b0ea49e70cad26ef5df6c8
knightchaser <agerio100@naver.com>
2025-11-20 11:47:49 +0900

    Docs: Set help annotation on user.nix

--- user.nix
+++ user.nix
@@ -1,6 +1,6 @@
 # user.nix
 
 {
-  username = "forshaw";
-  homeDirectory = "/home/forshaw";
+  username = "forshaw";             # Set your current Linux username here
+  homeDirectory = "/home/forshaw";  # Set your home directory path here (generally /home/${username})
 }
